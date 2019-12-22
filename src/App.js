259ff6050d4fd51ab931e9c218d9c3b0b8d3ebfc
Vaibhav Singh <svaibhavsingh@gmail.com>
2019-12-22 21:33:15 +0530

    fixing url

--- src/App.js
+++ src/App.js
@@ -45,10 +45,10 @@ class App extends Component {
     return (
       <div className="App">
         <Header data={this.state.resumeData.main}/>
-        <About data={this.state.resumeData.main} baseUrl={this.state.baseUrl}/>
+        <About data={this.state.resumeData.main}/>
         <Resume data={this.state.resumeData.resume}/>
         <Portfolio data={this.state.resumeData.portfolio}/>
-        {/* <Testimonials data={this.state.resumeData.testimonials} baseUrl={this.state.baseUrl}/> */}
+        {/* <Testimonials data={this.state.resumeData.testimonials}/> */}
         <Contact data={this.state.resumeData.main}/>
         <Footer data={this.state.resumeData.main}/>
       </div>
