366fd0991ade944206cbbc9016dd0d722af2d3d0
Vaibhav Singh <svaibhavsingh@gmail.com>
2019-12-22 19:05:35 +0530

    almost done

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -8,6 +8,7 @@ class Contact extends Component {
       var street = this.props.data.address.street;
       var city = this.props.data.address.city;
       var state = this.props.data.address.state;
+      var country = this.props.data.address.country;
       var zip = this.props.data.address.zip;
       var phone= this.props.data.phone;
       var email = this.props.data.email;
@@ -35,45 +36,10 @@ class Contact extends Component {
 
          <div className="row">
             <div className="eight columns">
-
-               <form action="" method="post" id="contactForm" name="contactForm">
-					<fieldset>
-
-                  <div>
-						   <label htmlFor="contactName">Name <span className="required">*</span></label>
-						   <input type="text" defaultValue="" size="35" id="contactName" name="contactName" onChange={this.handleChange}/>
-                  </div>
-
-                  <div>
-						   <label htmlFor="contactEmail">Email <span className="required">*</span></label>
-						   <input type="text" defaultValue="" size="35" id="contactEmail" name="contactEmail" onChange={this.handleChange}/>
-                  </div>
-
-                  <div>
-						   <label htmlFor="contactSubject">Subject</label>
-						   <input type="text" defaultValue="" size="35" id="contactSubject" name="contactSubject" onChange={this.handleChange}/>
-                  </div>
-
-                  <div>
-                     <label htmlFor="contactMessage">Message <span className="required">*</span></label>
-                     <textarea cols="50" rows="15" id="contactMessage" name="contactMessage"></textarea>
-                  </div>
-
-                  <div>
-                     <button className="submit">Submit</button>
-                     <span id="image-loader">
-                        <img alt="" src="images/loader.gif" />
-                     </span>
-                  </div>
-					</fieldset>
-				   </form>
-
-           <div id="message-warning"> Error boy</div>
-				   <div id="message-success">
-                  <i className="fa fa-check"></i>Your message was sent, thank you!<br />
-				   </div>
+               <iframe 
+               src="https://docs.google.com/forms/d/e/1FAIpQLSfSk5ExTRQ4QnxFSenuf41lPQrnMoZPbb6xqlTSly1e_QyKCA/viewform?embedded=true" 
+               width="640" height="100%" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
            </div>
-
 
             <aside className="four columns footer-widgets">
                <div className="widget widget_contact">
@@ -82,32 +48,12 @@ class Contact extends Component {
 					   <p className="address">
 						   {name}<br />
 						   {street} <br />
-						   {city}, {state} {zip}<br />
+						   {city}, {state}<br />
+                     {country}, {zip}<br/>
+                     {email}<br/>
 						   <span>{phone}</span>
 					   </p>
 				   </div>
-
-               <div className="widget widget_tweets">
-                  <h4 className="widget-title">Latest Tweets</h4>
-                  <ul id="twitter">
-                     <li>
-                        <span>
-                        This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet.
-                        Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum
-                        <a href="#">http://t.co/CGIrdxIlI3</a>
-                        </span>
-                        <b><a href="#">2 Days Ago</a></b>
-                     </li>
-                     <li>
-                        <span>
-                        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam,
-                        eaque ipsa quae ab illo inventore veritatis et quasi
-                        <a href="#">http://t.co/CGIrdxIlI3</a>
-                        </span>
-                        <b><a href="#">3 Days Ago</a></b>
-                     </li>
-                  </ul>
-		         </div>
             </aside>
       </div>
    </section>
--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -5,21 +5,15 @@ class Portfolio extends Component {
 
     if(this.props.data){
       var projects = this.props.data.projects.map(function(projects){
-        var projectImage = 'images/portfolio/'+projects.image;
-        return <div key={projects.title} className="columns portfolio-item">
-           <div className="item-wrap">
-            <a href={projects.url} title={projects.title}>
-               <img alt={projects.title} src={projectImage} />
-               <div className="overlay">
-                  <div className="portfolio-item-meta">
-                 <h5>{projects.title}</h5>
-                     <p>{projects.category}</p>
-                  </div>
-                </div>
-              <div className="link-icon"><i className="fa fa-link"></i></div>
-            </a>
+        return <div className="list-group">
+        <a href={projects.url} className="list-group-item list-group-item-action flex-column align-items-start active">
+          <div className="d-flex w-100 justify-content-between">
+            <h5 className="mb-1">{projects.title}</h5>
           </div>
-        </div>
+          <p className="mb-1">{projects.description}</p>
+          <small>{projects.period}</small>
+        </a>
+      </div>
       })
     }
 
