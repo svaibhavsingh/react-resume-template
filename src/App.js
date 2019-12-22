b226987786c7cd68d4d356afcbe226ac11613e97
Vaibhav Singh <svaibhavsingh@gmail.com>
2019-12-22 23:11:51 +0530

    pushing latest changes

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ class App extends Component {
 
   getResumeData(){
     $.ajax({
-      url:'website/resumeData.json',
+      url:'website/resumeData.json/',
       dataType:'json',
       cache: false,
       success: function(data){
