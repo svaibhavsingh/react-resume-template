6d60cd54ae845b21025cd4ef2201cfbc973804bd
Vaibhav Singh <svaibhavsingh@gmail.com>
2019-12-22 15:30:58 +0530

    initial modification

--- src/Components/About.js
+++ src/Components/About.js
@@ -10,6 +10,7 @@ class About extends Component {
       var street = this.props.data.address.street;
       var city = this.props.data.address.city;
       var state = this.props.data.address.state;
+      var country = this.props.data.address.country;
       var zip = this.props.data.address.zip;
       var phone= this.props.data.phone;
       var email = this.props.data.email;
@@ -20,7 +21,7 @@ class About extends Component {
       <section id="about">
       <div className="row">
          <div className="three columns">
-            <img className="profile-pic"  src={profilepic} alt="Tim Baker Profile Pic" />
+            <img className="profile-pic"  src={profilepic} alt="Vaibhav Singh Profile Pic" />
          </div>
          <div className="nine columns main-col">
             <h2>About Me</h2>
@@ -32,7 +33,8 @@ class About extends Component {
                   <p className="address">
 						   <span>{name}</span><br />
 						   <span>{street}<br />
-						         {city} {state}, {zip}
+						         {city}, {state},<br/>
+                           {country}, {zip}
                    </span><br />
 						   <span>{phone}</span><br />
                      <span>{email}</span>
