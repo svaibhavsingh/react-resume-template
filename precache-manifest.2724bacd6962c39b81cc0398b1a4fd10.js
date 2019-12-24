1afb8c75039e597db71af18cc5debdadfa1cd2f3
Vaibhav Singh <svaibhavsingh@gmail.com>
2019-12-24 22:47:36 +0530

    adding status file

---
+++ precache-manifest.2724bacd6962c39b81cc0398b1a4fd10.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "921a994800a58060441e126d5fb2439c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "67847545ef80b67da757",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "fdb76a46f7c69893a3ab",
+    "url": "/static/js/2.00000fb9.chunk.js"
+  },
+  {
+    "revision": "a1afa5f7c8dff2a316c41be2103336f5",
+    "url": "/static/js/2.00000fb9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "67847545ef80b67da757",
+    "url": "/static/js/main.36907b2a.chunk.js"
+  },
+  {
+    "revision": "60ff43ce3f4bc78af315",
+    "url": "/static/js/runtime-main.658afbb5.js"
+  }
+]);
